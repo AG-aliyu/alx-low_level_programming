4e4ecdb03d90c549136323566a3b6bde4fc572cd
AG-aliyu <agathinktwice123@gmail.com>
2022-03-20 06:07:32 -0700

    Task 3 alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+char lowerCase;
+char upperCase;
+for (upperCase = 'A'; upperCase <= 'Z'; upperCase++)
+{
+putchar(upperCase);
+}
+for (lowerCase = 'a'; lowerCase <= 'z'; lowerCase++)
+{
+putchar(lowerCase);
+}
+putchar("\n");
+return (0);
+}
