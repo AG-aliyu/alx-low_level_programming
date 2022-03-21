39f60975f58a6f067a3c9de5a0d2612ede992655
AG-aliyu <agathinktwice123@gmail.com>
2022-03-20 17:39:18 -0700

    Task 5 Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+init i;
+
+for (i = 0; i <= 10; i++)
+{
+ printf("%i", i);
+}
+putchar ('\n');
+
+return (0);
