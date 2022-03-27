97621801634cfee2a47cb28638506f069ab9ace5
AG-aliyu <agathinktwice123@gmail.com>
2022-03-27 15:03:27 -0700

    Task 2 strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,18 +2,24 @@
 
 /**
  * _strncpy - a function that copies a string
- * @dest: copy to
+ * @dest:copy to
  * @src: copy from
- * @n: input number of char
+ * @n: n
  * Return: Always 0 (Success)
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 int i;
 for (i = 0; src[i] != '\0'; i++)
-if (i < n)			
+{
+if (i < n)
+{
 dest[i] = src[i];
+}
+}
 while (i < n)
+{
 dest[i++] = '\0';
+}
 return (dest);
 }
