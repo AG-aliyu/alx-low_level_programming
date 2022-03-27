6642f7bdfa8429956951b43d01e744d9bd38f286
AG-aliyu <agathinktwice123@gmail.com>
2022-03-27 06:55:42 -0700

    Tast 1 strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,15 +8,24 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-int i, co;
-for (i = 0; dest[i] != '\0'; i++)
+int i, j;
+
+i =0;
+while (dest[i] != '\0')
 {
+i++;
 }
-for (co = 0; co < n; co++)
+
+j = 0;
+while(j < n  && src[j] !- '\0')
 {
-dest[i + co] = src[co];
-if (src[co] == '\0')
-co = n;
+dest[i] = src[j};
+j++;
+i++;
 }
+
+dest[i] = '\0';
+
 return (dest);
 }
+
