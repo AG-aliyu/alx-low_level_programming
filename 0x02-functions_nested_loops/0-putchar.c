2f25af43b58f3cadb427191bfc99b84e72c4c909
AG-aliyu <agathinktwice123@gmail.com>
2022-03-29 15:18:35 -0700

    Task 0 _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+_putchar('F');
+_putchar('U');
+_putchar('N');
+_putchar('C');
+_putchar('T');
+_putchar('I');
+_putchar('O');
+_putchar('N');
+_putchar('S');
+_putchar('A');
+_putchar('N');
+_putchar('D');
+_putchar('N');
+_putchar('E');
+_putchar('S');
+_putchar('T');
+_putchar('E');
+_putchar('D');
+_putchar('L');
+_putchar('O');
+_putchar('O');
+_putchar('P');
+_putchar('S');
+_putchar('\n');
+return (0);
+}
