dcdca283d6848894b8c1b0c53e2ab77d67ed348a
AG-aliyu <agathinktwice123@gmail.com>
2022-03-30 06:45:17 -0700

    Task 5 quare root of a number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - function square root of a number
+ * @n: integer to find sqrt of
+ *
+ * Return: natural square root or -1
+ */
+int _sqrt_recursion(int n)
+{
+if (n == 0)
+{
+return (0);
+}
+return (sqrt_check(1, n));
+}
